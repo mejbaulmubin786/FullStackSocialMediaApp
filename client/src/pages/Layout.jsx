5324b2970355686cf9b609f42b88b1f81dd814ce
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-11-05 21:53:05 +0600

    Update

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { Menu, X } from 'lucide-react'
+import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+import { dummyUserData } from '../assets/assets'
+import Sidebar from '../components/Sidebar'
+import Loding from '../components/Loding'
 
 const Layout = () => {
-    return (
-        <div>Layout</div>
+    const user = dummyUserData
+    const [sidebarOpen, setSidebarOpen] = useState(false)
+    return user ? (
+        <div className='w-full flex h-screen'>
+
+            <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+            <div className='flex-1 bg-slate-50'>
+                <Outlet />
+            </div>
+            {
+                sidebarOpen ?
+                    <X className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden' onClick={() => setSidebarOpen(false)} />
+                    :
+                    <Menu className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden' />
+            }
+        </div>
+    ) : (
+        <Loding />
     )
 }
 
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -10,7 +10,6 @@ const Login = () => {
             <img src={assets.bgImage} className='absolute top-0 left-0 -z-1 w-full h-full object-cover' />
 
 
-
             {/* left side : Branding */}
             <div className='flex-1 flex flex-col items-start justify-between p-6 md:p-10 lg:pl-40'>
                 <img src={assets.logo} alt="" className='h-12 object-contain' />
